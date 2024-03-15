c6e7cdc0dcf7e88a786e2b75208784cb348ca924
NicolasBonilla03 <inbonilla@uniboyaca.edu.co>
2024-03-14 23:45:39 -0500

    ARREGLOS GENERALES JS
    Se modifico el código eliminando cualquier ciclo dentro del mismo, se implemento una nueva función para capturar las posiciones del array, además de que se hizo uso de algunos métodos para capturar ciertos valores del array

--- Js/code.js
+++ Js/code.js
@@ -7,146 +7,94 @@ function array(){
     return arr;
 }
 
+function posicion(num){
+    const arr = array();
+    return arr.indexOf(num);
+}
+
 function mostrarPares(){
     const arr = array();
     const par = arr.filter((arr)=> arr%2==0);
     const formulario = document.getElementById('formulario');
-let html = '';
-formulario.innerHTML ='<h1>PARES</h1>';
-i=0;
-pars=0;
-while (i<200){
-    if (par[i]%2==0){
-formulario.innerHTML += '<h1>El numero '+par[i]+' se encuentra en la posicion '+(pars+1)+'</h1>';
-    }
-    i++;
-    pars=pars+2;
-}
-
+formulario.innerHTML ='<h1>PARES' +
+par.map((num)=>'<h2> Numero '+num+', en la posicion '+(posicion(num))+'.</h2>').join('')
++  '</h1>';
+var form = document.getElementById('form_num');
+form.innerHTML='';
 }
 
 function mostrarImpares(){
     const arr = array();
     const impar = arr.filter((arr)=> arr%2!=0);
-    const formulario = document.getElementById('formulario'); 
-    formulario.innerHTML +='<h1>IMPARES</h1>';
-i=0;
-imp=0
-while (i<100){
-    if (impar[i]%2!=0){
-        formulario.innerHTML += '<h1>El numero '+impar[i]+' se encuentra en la posicion '+(imp)+'</h1>';
-    }
-    i++;
-    imp=imp+2;
-}
-
+    const formulario = document.getElementById('formulario');
+formulario.innerHTML ='<h1>IMPARES' +
+impar.map((num)=>'<h2> Numero '+num+', en la posicion '+(posicion(num))+'.</h2>').join('')
++  '</h1>';
+var form = document.getElementById('form_num');
+form.innerHTML='';
 }
 
 function ParesMayor(){
-    var formulario = document.getElementById('form_num'); 
+    var form = document.getElementById('form_num');
+    form.innerHTML = '';
+    var formulario = form; 
 formulario.innerHTML +='<h1>PARES>#</h1>';
 formulario.innerHTML +='<input type="number" max="200" id="numero">';
 formulario.innerHTML +='<button type="button" onclick="numpar()" id="valor">Mirar</button>'
 
-
 }
 
 function numpar(){
-    const num = document.getElementById('numero').value;
+    var num = document.getElementById('numero').value;
+    if(num%2==0){
+        num= num/2;
+    }else{
+    num = (num/2);
+    }
     ParesMayorbtn(num);
-}
 
+}
 
 function ParesMayorbtn(num){
-    var numd = num;
-    numd = Math.ceil(num);
-    if (num%2==0){
-        num = num/2;
-    }else{
-        num = num/2;
-        num = Math.ceil(num);
-        num = num-1;
-    }
-    console.log(numd);
-    console.log(num);
-    const arr = array();
-    const par = arr.filter((arr)=> arr%2==0);
-    const formulario = document.getElementById('formulario'); 
-
-    formulario.innerHTML +='<h1>PARES</h1>';
-i=num;
-pars=0;
-if (numd%2==0){
-    pars=numd;
-while (pars<200){
-    if (par[i]%2==0){
-        formulario.innerHTML += '<h1>El numero '+par[i]+' se encuentra en la posicion '+((pars+1))+'</h1>';
-    }
-    i++;
-    pars=pars+2;
-}
-}else if(numd%2!=0){
-    pars=numd;
-    while (pars<200){
-        if (par[i]%2==0){
-            formulario.innerHTML += '<h1>El numero '+par[i]+' se encuentra en la posicion '+((pars))+'</h1>';
-        }
-        i++;
-        pars=pars+2;
-    }
-}
-
+    var arr = array();
+    parr = arr.filter((num)=> num%2==0);
+    par = parr.slice(num);
+    console.log(par)
+    const formulario = document.getElementById('formulario');
+formulario.innerHTML ='<h1>PARES' +
+par.map((num)=>'<h2> Numero '+num+', en la posicion '+(posicion(num))+'.</h2>').join('')
++  '</h1>';
+num=0;
 
 }
 function ImparesMayor(){
-    const formulario = document.getElementById('form_num'); 
+    var form = document.getElementById('form_num');
+    form.innerHTML = '';
+    var formulario = document.getElementById('form_num'); 
     formulario.innerHTML +='<h1>IMPARES>#</h1>';
     formulario.innerHTML +='<input type="number" max="200" id="numero">';
-    formulario.innerHTML +='<button type="button" onclick="numimp()" id="valor">Mirar</button>'
+    formulario.innerHTML +='<button type="button" onclick="numimp()" id="valor">Mirar</button>';
 
 }
 
 function numimp(){
-    const num = document.getElementById('numero').value;
+    var num = document.getElementById('numero').value;
+    if(num%2==0){
+        num= num/2;
+    }else{
+    num = (num/2)+1;
+    }
     ImparesMayorbtn(num);
 }
 
-
 function ImparesMayorbtn(num){
-    var numd = num;
-    numd = Math.ceil(num);
-    if (num%2==0){
-        num = num/2;
-    }else{
-        num = num/2;
-        num = Math.ceil(num);
-        num = num-1;
-    }
-    
-    console.log(num);
-    const arr = array();
-    const impar = arr.filter((arr)=> arr%2!=0);
-    const formulario = document.getElementById('formulario'); 
-formulario.innerHTML +='<h1>IMPARES>#</h1>';
-i=num;
-pars=0;
-if (numd%2!=0){
-    pars=numd;
-while (pars<199){
-    if (impar[i]%2!=0){
-        formulario.innerHTML += '<h1>El numero '+impar[i+1]+' se encuentra en la posicion '+((pars+1))+'</h1>';
-    }
-    i++;
-    pars=pars+2;
-}
-} else {
-    pars=numd;
-    while (pars<200){
-        if (impar[i]%2!=0){
-            formulario.innerHTML += '<h1>El numero '+impar[i]+' se encuentra en la posicion '+((pars))+'</h1>';
-        }
-        i++;
-        pars=pars+2;
-    }
-}
+    var arr = array();
+    imparr = arr.filter((num)=> num%2!=0);
+    impar = imparr.slice(num);
+    console.log(impar)
+    const formulario = document.getElementById('formulario');
+formulario.innerHTML ='<h1>IMPARES' +
+impar.map((num)=>'<h2> Numero '+num+', en la posicion '+(posicion(num))+'.</h2>').join('')
++  '</h1>';
+num=0;
 }
